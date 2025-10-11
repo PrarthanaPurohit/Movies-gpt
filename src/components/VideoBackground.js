f3f04cbd12220b0168233e7f563be0cbbafbe79c
Prarthana Purohit <prarthana.purohit03@gmail.com>
2025-10-11 20:28:55 +0530

    trailer UI

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,23 +1,26 @@
-
 import { useSelector } from "react-redux";
 import useTrailerVideo from "../hooks/useTrailerVideo";
 
-
 const VideoBackground = ({ movieId }) => {
-  const trailerVideo = useSelector(store => store.movies?.trailerVideo);
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useTrailerVideo(movieId);
-  
-  return <div><iframe width="560"
-   height="315" 
-   src={"https://www.youtube.com/embed/" + trailerVideo?.key}  //can use trailerVideo?.key
-    title="YouTube video player" 
-    frameborder="0"
-     allow="accelerometer;
+
+  return (
+    <div className="w-screen">
+      <iframe className="w-screen aspect-video"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"} //can use trailerVideo?.key
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer;
       autoplay; clipboard-write; 
       encrypted-media; gyroscope;
-       picture-in-picture; web-share" 
-       referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen></iframe></div>;
+       picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+        
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='pt-36 px-12'>
-        <h1 className='text-6xl font-bold'>{title}</h1>
+    <div className='w-screen aspect-video pt-[10%] px-24 absolute text-white bg-gradient-to-r from-black'>
+        <h1 className='text-5xl font-bold'>{title}</h1>
         <p className='py-6 text-lg w-1/4 text-justify leading-relaxed'>{overview}</p>
         <div className='flex space-x-4'>
-            <button className='bg-red-500 hover:bg-red-800 text-black font-semibold py-2 px-6 text-lg  rounded-md transition'>Play</button>
-            <button className='bg-gray-500 hover:bg-gray-500 text-white font-semibold py-2 px-6 text-lg opacity-70 rounded-md transition'>More info</button>
+            <button className='bg-white hover:bg-opacity-70 text-black font-semibold py-2 px-6 text-lg  opacity-80 rounded-md transition'>Play</button>
+            <button className='bg-gray-400 hover:bg-opacity-50 text-white font-semibold py-2 px-6 text-lg opacity-50 rounded-md transition'>More info</button>
         </div>
     </div>
   )
