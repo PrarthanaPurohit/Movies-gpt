92f20b0e56a2666743959979922435e43a1fcecb
Prarthana Purohit <prarthana.purohit03@gmail.com>
2025-10-11 16:21:28 +0530

    updated store with api data

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,13 @@
-import React, { useEffect } from "react";
+
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  //call the hook
+  useNowPlayingMovies();
+
+    
 
   return (
     <div>
